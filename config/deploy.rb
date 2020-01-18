00a5996c162c2e5ff6ccf349baf944ec44e0ab08
EC2 Default User <ec2-user@ip-172-31-31-214.ap-southeast-1.compute.internal>
2020-01-18 12:23:59 +0000

    last config complete

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, 'master'
 
 set :deploy_to, '/var/www/techweb'
 
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
